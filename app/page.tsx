39613c869492effcf7c0f2b815337d57761d8337
Sridhar-FullStack-Dev <sridhar.fullstack.dev.india@gmail.com>
2024-12-03 14:10:14 +0530

    cursor

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 "use client";
+import Cursor from "@/components/Cursor/Cursor";
 import Hero from "@/components/Hero/Hero";
 import Intro from "@/components/Intro/Intro";
 
 export default function Home() {
   return (
     <div>
+      <Cursor />
       <Intro />
       <Hero />
     </div>
