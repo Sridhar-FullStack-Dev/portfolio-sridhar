6c8056796e300bc7909b6d0a48e31cb14e3595de
Sridhar-FullStack-Dev <sridhar.fullstack.dev.india@gmail.com>
2024-12-04 17:29:35 +0530

    h

--- components/Intro/Intro.tsx
+++ components/Intro/Intro.tsx
@@ -30,7 +30,7 @@ export default function Intro() {
         }
         return prev + 1;
       });
-    }, 10);
+    }, 30);
 
     return () => {
       clearTimeout(showTimer);
@@ -48,20 +48,19 @@ export default function Intro() {
           transition={{ ease: "easeOut", duration: 1 }}
           className="h-screen w-screen fixed z-[99999] flex justify-center items-center overflow-hidden bg-white milker-font"
         >
-          <div className="absolute inset-0 bg-alt-black animate-[slideIn_3s_ease-in]" />
+          <div className="absolute inset-0 bg-black animate-[slideIn_3s_ease-in]" />
           <div className="flex flex-col items-center relative z-10">
             <motion.h1
+              key={text}
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
-              transition={{ ease: "easeInOut", duration: 0.4 }}
-              className={`text-8xl transition-colors duration-300 relative ${
+              exit={{ opacity: 0 }}
+              transition={{ ease: "easeOut", duration: 0.4 }}
+              className={`text-8xl transition-colors duration-300 ${
                 text === "portfolio" ? "text-white" : "text-black"
               }`}
             >
               {text}
-              <span className="text-4xl font-sans absolute -right-10 top-0">
-                &reg;
-              </span>
             </motion.h1>
             <span
               className={`text-8xl mt-4 transition-colors duration-300 fixed bottom-10 right-10 ${
