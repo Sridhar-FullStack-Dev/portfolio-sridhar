713269ca19ab5936d929110ed89f99c0432b51b3
Sridhar-FullStack-Dev <sridhar.fullstack.dev.india@gmail.com>
2024-12-21 11:09:29 +0530

    about changes

--- components/About/AboutScroll/AboutScroll.tsx
+++ components/About/AboutScroll/AboutScroll.tsx
@@ -19,28 +19,10 @@ export default function AboutScroll() {
   return (
     <div ref={aboutLocoRef} data-scroll-container className="px-32">
       <div className="flex justify-between items-center relative">
-        <div data-scroll data-scroll-speed="0.2" className="relative w-1/2">
-          <Image
-            src={"/About/1.jpg"}
-            alt=""
-            height={400}
-            width={400}
-            className="w-96"
-          />
-        </div>
-
-        <div
-          data-scroll
-          data-scroll-speed="0.3"
-          className="text-8xl absolute -left-20 top-1/2 pl-4"
-        >
-          <h1>Mission</h1>
-        </div>
-
-        <div className="relative w-1/2 mt-64">
-          <div data-scroll data-scroll-speed="0.3">
+        <div className="relative">
+          <div data-scroll data-scroll-speed="0.2">
             <Image
-              src={"/About/2.jpg"}
+              src={"/About/1.jpg"}
               alt=""
               height={400}
               width={400}
@@ -50,34 +32,11 @@ export default function AboutScroll() {
 
           <div
             data-scroll
-            data-scroll-speed="0.4"
-            className="text-8xl absolute left-[20%] bottom-72 pl-4"
+            data-scroll-speed="0.3"
+            className="absolute top-1/2 -left-10 text-8xl"
           >
-            <h1>Vision</h1>
+            Mission
           </div>
-        </div>
-
-        <div></div>
-      </div>
-
-      <div className="flex justify-end items-center"></div>
-
-      <div className="flex justify-start items-center">
-        <div data-scroll data-scroll-speed="0.4" className="relative">
-          <Image
-            src={"/About/3.jpg"}
-            alt=""
-            height={400}
-            width={400}
-            className="w-96"
-          />
-          <h1
-            data-scroll
-            data-scroll-speed="0.5"
-            className="text-8xl absolute right-1/2 top-1/2 pl-4"
-          >
-            Vision
-          </h1>
         </div>
       </div>
     </div>
