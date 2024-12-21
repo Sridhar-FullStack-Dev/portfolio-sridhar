7c22daecfac6696b3b6b7afaa65f43a38b0fd17b
Sridhar-FullStack-Dev <sridhar.fullstack.dev.india@gmail.com>
2024-12-21 12:00:56 +0530

    about

--- components/About/AboutScroll/AboutScroll.tsx
+++ components/About/AboutScroll/AboutScroll.tsx
@@ -1,8 +1,7 @@
-import Image from "next/image";
-import { useEffect, useRef } from "react";
 import LocomotiveScroll, { ILocomotiveScrollOptions } from "locomotive-scroll";
+import { useEffect, useRef } from "react";
 import ParallaxSection from "./ParallaxSection/ParallaxSection";
-const TOTAL_IMAGES = 10;
+import QuoteSection from "./QuoteSection/QuoteSection";
 
 export default function AboutScroll() {
   const aboutLocoRef = useRef<HTMLHeadingElement>(null);
@@ -17,32 +16,11 @@ export default function AboutScroll() {
       if (scroll) scroll.destroy();
     };
   }, []);
-
-  const imageGrid = Array.from(
-    { length: TOTAL_IMAGES },
-    (_, index) => index + 1
-  );
-
   return (
     <div ref={aboutLocoRef} data-scroll-container className="px-32">
       <ParallaxSection />
 
-      <div className="grid grid-cols-5 relative h-screen">
-        <h1 className="text-[19rem] tracking-wide milker-font font-bold text-center uppercase w-full absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 opacity-75">
-          dreams
-        </h1>
-
-        {imageGrid.map((index) => (
-          <Image
-            key={index}
-            src={`/About/${index}.jpg`}
-            alt=""
-            height={300}
-            width={300}
-            className="w-80"
-          />
-        ))}
-      </div>
+      <QuoteSection />
     </div>
   );
 }
---
+++ components/About/AboutScroll/QuoteSection/QuoteSection.tsx
@@ -0,0 +1,93 @@
+import gsap from "gsap";
+import LocomotiveScroll, { ILocomotiveScrollOptions } from "locomotive-scroll";
+import Image from "next/image";
+import { useEffect, useRef } from "react";
+
+const TOTAL_IMAGES = 10;
+export default function QuoteSection() {
+  const imagesRef = useRef<(HTMLImageElement | null)[]>([]);
+  const headingRef = useRef<HTMLHeadingElement>(null);
+
+  const imageGrid = Array.from(
+    { length: TOTAL_IMAGES },
+    (_, index) => index + 1
+  );
+  useEffect(() => {
+    gsap.fromTo(
+      headingRef.current,
+      {
+        opacity: 0,
+        scale: 0.8,
+        y: 100,
+      },
+      {
+        opacity: 1,
+        scale: 1,
+        y: 0,
+        duration: 1.5,
+        ease: "power4.out",
+        scrollTrigger: {
+          trigger: headingRef.current,
+          start: "top center+=100",
+          toggleActions: "play none none reverse",
+        },
+      }
+    );
+  }, []);
+
+  useEffect(() => {
+    const scroll = new LocomotiveScroll({
+      el: document.querySelector("[data-scroll-container]") as HTMLElement,
+      smooth: true,
+    } as ILocomotiveScrollOptions);
+
+    imagesRef.current.forEach((image, index) => {
+      gsap.fromTo(
+        image,
+        {
+          opacity: 0,
+          y: 100,
+        },
+        {
+          opacity: 1,
+          y: 0,
+          duration: 1,
+          ease: "power3.out",
+          scrollTrigger: {
+            trigger: image,
+            start: "top bottom-=100",
+            toggleActions: "play none none reverse",
+          },
+          delay: index * 0.2, // Stagger effect
+        }
+      );
+    });
+
+    return () => {
+      if (scroll) scroll.destroy();
+    };
+  }, []);
+
+  return (
+    <div className="grid grid-cols-5 relative h-screen">
+      <h1
+        ref={headingRef}
+        className="text-[7rem] leading-none z-10 tracking-wide milker-font font-bold text-center uppercase w-full absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 opacity-75"
+      >
+        The best way to predict the future is to create it
+      </h1>
+
+      {imageGrid.map((index) => (
+        <Image
+          key={`about-image-${index}`}
+          ref={(el: any) => (imagesRef.current[index - 1] = el)}
+          src={`/About/${index}.jpg`}
+          alt={`About Image ${index}`}
+          height={300}
+          width={300}
+          className="w-80 opacity-0"
+        />
+      ))}
+    </div>
+  );
+}
