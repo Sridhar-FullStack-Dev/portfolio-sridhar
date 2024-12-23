250f16a3155a85328fb753c2c31de890699f09ae
Sridhar-FullStack-Dev <sridhar.fullstack.dev.india@gmail.com>
2024-12-23 18:31:09 +0530

    b

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -4,6 +4,7 @@ import { useEffect, useRef, useState } from "react";
 import { IoIosArrowDown } from "react-icons/io";
 import SplitType from "split-type";
 import { Vortex } from "../ui/vortex";
+import Image from "next/image";
 
 export default function Hero() {
   const [show, setShow] = useState<boolean>(false);
@@ -36,45 +37,47 @@ export default function Hero() {
   }, []);
 
   return (
-    <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ ease: "easeInOut", duration: 1 }}
-      className="h-screen w-screen overflow-hidden"
-    >
-      {show && (
-        <Vortex
-          backgroundColor="transparent"
-          className="flex items-center flex-col justify-center px-2 md:px-10 py-4 w-full h-full"
-        >
-          <motion.h1
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ ease: "easeIn", delay: 2 }}
-            ref={textRef}
-            className="px-32 text-8xl text-center houseMontage-font uppercase"
-          >
-            Hurray!
-          </motion.h1>
-          <motion.p
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ ease: "easeIn", delay: 2.2 }}
-            className="text-2xl text-center w-96 sarcolenta-font tracking-wider capitalize"
-          >
-            You've reached the right spot to develop your business idea to the
-            world wide
-          </motion.p>
+    // <motion.div
+    //   initial={{ opacity: 0 }}
+    //   animate={{ opacity: 1 }}
+    //   transition={{ ease: "easeInOut", duration: 1 }}
+    //   className="h-screen w-screen overflow-hidden"
+    // >
+    //   {show && (
+    //     <Vortex
+    //       backgroundColor="transparent"
+    //       className="flex items-center flex-col justify-center px-2 md:px-10 py-4 w-full h-full"
+    //     >
+    //       <motion.h1
+    //         initial={{ opacity: 0 }}
+    //         animate={{ opacity: 1 }}
+    //         transition={{ ease: "easeIn", delay: 2 }}
+    //         ref={textRef}
+    //         className="px-32 text-8xl text-center houseMontage-font uppercase"
+    //       >
+    //         Hurray!
+    //       </motion.h1>
+    //       <motion.p
+    //         initial={{ opacity: 0 }}
+    //         animate={{ opacity: 1 }}
+    //         transition={{ ease: "easeIn", delay: 2.2 }}
+    //         className="text-2xl text-center w-96 sarcolenta-font tracking-wider capitalize"
+    //       >
+    //         You've reached the right spot to develop your business idea to the
+    //         world wide
+    //       </motion.p>
 
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ ease: "easeIn", delay: 2.5 }}
-          >
-            <IoIosArrowDown className="animate-bounce mt-5 text-xl" />
-          </motion.div>
-        </Vortex>
-      )}
-    </motion.div>
+    //       <motion.div
+    //         initial={{ opacity: 0 }}
+    //         animate={{ opacity: 1 }}
+    //         transition={{ ease: "easeIn", delay: 2.5 }}
+    //       >
+    //         <IoIosArrowDown className="animate-bounce mt-5 text-xl" />
+    //       </motion.div>
+    //     </Vortex>
+    //   )}
+    // </motion.div>
+
+    <div className="h-screen w-screen"></div>
   );
 }
--- components/Services/CardCarousel/CardCarousel.tsx
+++ components/Services/CardCarousel/CardCarousel.tsx
@@ -1,7 +1,7 @@
-import Image from "next/image";
-import { useEffect, useRef } from "react";
 import gsap from "gsap";
 import ScrollTrigger from "gsap/dist/ScrollTrigger";
+import Image from "next/image";
+import { useEffect, useRef } from "react";
 
 export default function CardCarousel() {
   const sectionRef = useRef<HTMLDivElement>(null);
@@ -79,22 +79,22 @@ interface CardCarouselData {
 
 const cardCarouselData: CardCarouselData[] = [
   {
-    src: "https://images.pexels.com/photos/4195342/pexels-photo-4195342.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    heading: "Web development",
+    src: "https://images.pexels.com/photos/1779487/pexels-photo-1779487.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    heading: "Web designing",
     description: "",
   },
   {
-    src: "https://images.pexels.com/photos/2102416/pexels-photo-2102416.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    src: "https://images.pexels.com/photos/230544/pexels-photo-230544.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     heading: "Web Application development",
     description: "",
   },
   {
-    src: "https://images.pexels.com/photos/2047914/pexels-photo-2047914.jpeg?auto=compress&cs=tinysrgb&w=600",
-    heading: "Branding",
+    src: "https://images.pexels.com/photos/7688336/pexels-photo-7688336.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    heading: "SEO",
     description: "",
   },
   {
-    src: "https://images.pexels.com/photos/2047914/pexels-photo-2047914.jpeg?auto=compress&cs=tinysrgb&w=600",
+    src: "https://img.freepik.com/free-vector/hand-drawn-illustrations-collection_1096-260.jpg?t=st=1734958081~exp=1734961681~hmac=d82b862c269e501c27c9bbc1baeea6be42a4900b11b12c6d0e50531fcddf06c8&w=740",
     heading: "Digital Marketing",
     description: "",
   },
