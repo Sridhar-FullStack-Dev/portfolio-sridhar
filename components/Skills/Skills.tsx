46db8a2630cb32ee9438609467a2f4e3a68e2c6f
Sridhar-FullStack-Dev <sridhar.fullstack.dev.india@gmail.com>
2025-01-24 17:18:26 +0530

    skills

--- components/Skills/Skills.tsx
+++ components/Skills/Skills.tsx
@@ -45,28 +45,15 @@ export function Skills() {
 
   return (
     <div className="relative flex w-full flex-col items-center justify-center overflow-hidden rounded-lg bg-alt-black mt-20 mb-52">
-      <div className="flex justify-start items-center w-full pl-16">
+      <div className="flex justify-start items-center w-full pl-16 relative">
         <h1
           ref={skillsTextRef}
           className="milker-font text-left text-8xl uppercase perspective-1000 text-alt-white"
         >
           Skills
         </h1>
+        <div className="pointer-events-none absolute inset-y-0 left-0 w-1/5 bg-gradient-to-r from-alt-black"></div>
       </div>
-      <Marquee className="[--duration:50s] mt-8 relative z-20">
-        {Array.from({ length: 3 }).map((_, index) => (
-          <SkillCardOne key={index} />
-        ))}
-      </Marquee>
-      <Marquee reverse className="[--duration:50s] relative z-20">
-        {Array.from({ length: 3 }).map((_, index) => (
-          <SkillCardTwo key={index} />
-        ))}
-      </Marquee>
-      <div className="pointer-events-none absolute inset-y-0 left-0 w-1/5 bg-gradient-to-r from-alt-black"></div>
-      <div className="pointer-events-none absolute inset-y-0 right-0 w-1/3 bg-gradient-to-l from-alt-black"></div>
-
-      <RetroGrid />
     </div>
   );
 }
